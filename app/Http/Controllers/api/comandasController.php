9b6e6491e55b1b2fe950c0cfb3f68261e72625c0
Victor <victor.porras.work@gmail.com>
2023-01-09 14:13:07 +0100

    actu victor rama

---
+++ app/Http/Controllers/api/comandasController.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Comanda;
+
+class comandasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $comandas = Comanda::all(['id','nom']);
+        $response = [
+            'success' => true,
+            'message' => "Llistat comandes recuperat",
+            'data' => $comandas,
+        ];
+        //return $response;
+        return response()->json($response,200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $input = $request->all();
+
+        $comanda = Comanda::create($input);
+
+        return $comanda;
+
+        //return $input;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $comanda = Comanda::find($id);
+        if($comanda==null){
+            
+            $response = [
+                'success' => false,
+                'message' => "Comanda no recuperada",
+                'data' => [],
+            ];
+ 
+            return response()->json($response,404);
+        }
+     
+            $response = [
+                'success' => true,
+                'message' => "Comanda recuperada",
+                'data' => $comanda,
+            ];
+        
+            return response()->json($response,200);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $comanda = Comanda::find($id);
+        if($comanda==null){
+            
+            $response = [
+                'success' => false,
+                'message' => "Comanda no recuperada",
+                'data' => [],
+            ];
+ 
+            return response()->json($response,404);
+        }
+     
+        try{
+            $comanda->delete();
+
+            $response = [
+                'success' => true,
+                'message' => "Comanda esborrada",
+                'data' => $comanda,
+            ];
+        
+            return response()->json($response,200);
+
+        }
+
+        catch(\Excepetion $e){
+
+            $response = [
+                'success' => false,
+                'message' => "Error esborrant comanda",
+                
+            ];
+        
+            return response()->json($response,400);
+
+        }
+       
+
+    
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/hola',function() {
+    echo "Hola";
+});
+
+//Route::get('/comandas', [App\Http\Controllers\api\comandasController::class, 'index']);
+//Route::get('/comandas/{id}', [App\Http\Controllers\api\comandasController::class, 'show']);
+//Route::delete('/comandas/{id}', [App\Http\Controllers\api\comandasController::class, 'destroy']);
+//Route::post('/comandas/{id}', [App\Http\Controllers\api\comandasController::class, 'store']);
+//Route::put('/comandas/{id}', [App\Http\Controllers\api\comandasController::class, 'update']);
+
+Route::resource('/comandas',App\Http\Controllers\api\comandasController::class);
